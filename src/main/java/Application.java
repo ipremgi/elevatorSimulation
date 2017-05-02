3edfacd0d5ad32429e8af2361571a8ccaf69ba5a
Premgi <ivan.premgi@capgemini.com>
2017-05-02 12:59:27 +0100

    project structure

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IPREMGI on 02/05/2017.
+ */
+public class Application {
+
+
+    /**
+     * MAIN CLASS - PLACEHOLDER
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
