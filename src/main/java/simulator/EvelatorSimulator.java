e909d910170958da4f0d7fa9a3cc66fa48f5c13f
Ivan <premgii@aston.ac.uk>
2017-05-02 13:11:10 +0100

    files added to show folder

---
+++ src/main/java/simulator/EvelatorSimulator.java
@@ -0,0 +1,10 @@
+package simulator;
+
+/**
+ * Created by IPREMGI on 02/05/2017.
+ */
+public class EvelatorSimulator implements ISimulator {
+    public void simulate() {
+
+    }
+}
---
+++ src/main/java/simulator/ISimulator.java
@@ -0,0 +1,9 @@
+package simulator;
+
+/**
+ * Created by IPREMGI on 02/05/2017.
+ */
+public interface ISimulator {
+
+    void simulate();
+}
