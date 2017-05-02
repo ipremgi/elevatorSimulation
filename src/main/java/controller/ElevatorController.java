378f6d4526e58602904b61e271db55960bf426c7
Wilkhu <hardish.wilkhu@capgemini.com>
2017-05-02 15:44:08 +0100

    added some logic to methods

--- src/main/java/controller/ElevatorController.java
+++ src/main/java/controller/ElevatorController.java
@@ -3,6 +3,8 @@ package controller;
 import view.ElevatorView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by HWILKHU on 02/05/2017.
@@ -11,46 +13,121 @@ public class ElevatorController {
 
     private Elevator elevator;
     private ElevatorView elevatorView;
+    private Building building;
     private int ticks;
-    private Arraylist<elevatoruser> buildingOccupants= new ArrayList<ElevatorUser>();
+    private Arraylist<ElevatorUser> buildingOccupants= new ArrayList<ElevatorUser>();
+    private DoorStatus doorStatus;
+    private Direction direction;
 
-    public ElevatorController(Elevator elevator, ElevatorView elevatorView, int numMugstoneEmployees, int numGoggleEmployees) {
+    public ElevatorController(Elevator elevator, ElevatorView elevatorView, Building building, int numMugstoneEmployees, int numGoggleEmployees) {
         this.elevator = elevator;
         this.elevatorView = elevatorView;
+        this.building = building;
+
+
     }
 
-    public void nextTick(){}
+    public void nextTick(){
 
-    private void openElevatorDoor(){}
 
-    private void closeElevatorDoor(){}
+        elevatorView.updateView();
+    }
 
-    private void moveElevator(){}
+    private void openElevatorDoor(){
+        elevator.setDoorStatus(doorStatus.OPEN);
+    }
+
+    private void closeElevatorDoor(){
+        elevator.setDoorStatus(doorStatus.CLOSED);
+    }
+
+    private void moveElevator(){
+        elevator.setFloor=calculateNextFloor();
+    }
 
     private int usedCapacity(){
+        int usedCapacity = 0;
 
-        return 0;
+        List<ElevatorUser> elevatorOccupants = elevator.getElevatorOccupants();
+
+        for (ElevatorUser occupant : elevatorOccupants){
+            usedCapacity = usedCapacity + occupant.getCapacity();
+        }
+
+        return usedCapacity;
     }
 
-    private boolean canAddPersonToElevator(){
+    private boolean canAddPersonToElevator(ElevatorUser person){
+
+        if ((person.getCapacity() + usedCapacity()) > elevator.getMaxCap()){
+            return false;
+        }
 
         return true;
     }
 
-    private void addPersonToElevator(){}
-
-    private int calculateNextFloor(){
-
-        return 0;
+    private void addPersonToElevator(ElevatorUser person){
+        elevator.addPerson(person);
     }
 
-    private void leaveEelevator(){}
+    private int calculateNextFloor(){
+        ArrayList<Integer> requests = checkForRequests();
+        int nextFloor = 0;
 
-    private void noRequsts(){}
+        for (ElevatorUser occupant : elevatorOccupants){
+            requests.add(occupant.getDestFloor);
+        }
 
-    private int[] checkForRequests(){
+        if (elevator.getDirection = direction.UP){
+            for (int floorNumber : requests){
+                if(floorNumber < elevator.getFloor()){
+                    requests.remove(floorNumber);
+                }
+            }
 
-        return {0};
+            nextFloor = requests.indexOf(Collections.min(requests));
+
+        } else if (elevator.getDirection = direction.DOWN){
+            for (int floorNumber : requests){
+                if(floorNumber > elevator.getFloor()){
+                    requests.remove(floorNumber);
+                }
+            }
+
+            nextFloor = requests.indexOf(Collections.max(requests));
+
+        }
+
+        return nextFloor;
+    }
+
+    private void leaveEelevator(){
+        List<ElevatorUser> elevatorOccupants = elevator.getElevatorOccupants();
+
+        for (ElevatorUser occupant : elevatorOccupants){
+            if (occupant.getDestFloor() == elevator.getFloor()){
+                elevator.removePerson(occupant);
+            }
+        }
+
+    }
+
+    private void noRequsts(){
+        elevator.setFloor(0);
+    }
+
+    private ArrayList<Integer> checkForRequests(){
+        List<Floor> floors = buiilding.getFloors();
+
+        ArrayList<Integer> floorRequests = new ArrayList<Integer>();
+
+        for (Floor floor : floors){
+            if (floor.isButtonPressed()){
+                floorRequests.add(floor.getFloorNumber());
+            }
+        }
+
+        return floorRequests;
     }
 
 }
