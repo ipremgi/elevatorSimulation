fc8591fce62071d3b3c3ab329a287b112fe6ccb8
aishwarya-kandukuri <apsara.aishwarya@gmail.com>
2017-05-11 11:26:07 +0100

    issues resolved. - implemented tick class -added Atomicid - resolved issues on git

--- src/main/java/model/user/Developer.java
+++ src/main/java/model/user/Developer.java
@@ -11,40 +11,36 @@ public class Developer extends ElevatorUser {
     private Company company;
     private Random randomGenerator;
 
-    public Developer(Company company, Integer capacity, Integer destFloor, double probability ) {
+    public Developer(Company company, int capacity, int destFloor, double probability, int priority, int maxFloors  ) {
+        super("developer" + company.name());
         this.company = company;
         this.setCapacity(capacity);
         this.setProbabilty(probability);
         this.setDestFloor(destFloor);
+        this.setPriority(priority);
+        this.setMaxFloors(maxFloors);//for rest of the classes
+        this.setFloorsAccessible(determineFloorsAccessible());
+    }
 
+    @Override
+    protected List<Integer> determineFloorsAccessible(){
         List<Integer> floorsAccessible = new ArrayList<Integer>();
-
         for (int i = getMaxFloors(); i <= getMaxFloors() / 2; i--) {
             floorsAccessible.add(i);
         }
-
-        this.setFloorsAccessable(floorsAccessible);
+        return floorsAccessible;
     }
 
     public void leaveBuilding() {
         setDestFloor(0);
     }
 
-
     public void moveFloor() {
-
-        int randomFloor = randomGenerator.nextInt(getFloorsAccessable().size());
-        this.setDestFloor( getFloorsAccessable().get(randomFloor));
-
+        int randomFloor = randomGenerator.nextInt(getFloorsAccessible().size());
+        this.setDestFloor( getFloorsAccessible().get(randomFloor));
         //only work in the top half of the building. Developers may randomly decide to move to another floor in the top half.
     }
 
-
-
-
-
-
-
-    }
+}
 
 
