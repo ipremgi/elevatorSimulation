94e79043cc2780618c413392ab88f77b4506b7ea
hardishw <hardish_95@hotmail.co.uk>
2017-05-14 17:04:00 +0100

    added abstract class for users that leave after a amount of time

---
+++ src/main/java/model/user/Leaves.java
@@ -0,0 +1,48 @@
+package model.user;
+
+import java.util.List;
+
+/**
+ * Created by HWILKHU on 14/05/2017.
+ */
+public abstract class Leaves extends ElevatorUser {
+
+    private int tickDuration;
+    private boolean leaving = false;
+    private boolean removeMe = false;
+
+    public Leaves(String className) {
+        super(className);
+    }
+
+    @Override
+    public void moveFloor() {
+
+    }
+
+    public void leaveBuilding() {
+        setDestFloor(0);
+    }
+
+    public void shouldILeave() {
+        //check that they have reached their randomly assigned time
+        if (getCurrentFloor() == getDestFloor() && !leaving){
+            tickDuration--;
+            if (tickDuration == 0) {
+                leaving = true;
+                leaveBuilding();
+            }
+        }else if(getCurrentFloor() == getDestFloor() && leaving){
+            this.removeMe = true;
+        }
+    }
+
+    public boolean isRemoveMe() {
+        return removeMe;
+    }
+
+    public void setTickDuration(int tickDuration) {
+        this.tickDuration = tickDuration;
+    }
+
+}
